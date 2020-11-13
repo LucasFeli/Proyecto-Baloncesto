138b03bd48cf6a9862c5f5fa24f788a43a31ee62
LucasFeli <felipe298@gmail.com>
2020-11-13 22:16:49 +0100

    cambio 2

--- balls.js
+++ balls.js
@@ -2,14 +2,14 @@
 
 class Balls {
     constructor(canvas, x) {
-        this.size = 60;
+        this.size = 80;
         this.canvas = canvas;
         this.ctx = this.canvas.getContext("2d");
         this.x = x;
-        this.y = 2;
+        this.y = 1;
         this.speed = 3;
         this.direction = -1;
-        this.image = "./images/Balon.png"
+        this.image = "./images/76179-image_450x450.png"
       }
     
       update() {
@@ -21,7 +21,7 @@ class Balls {
       draw() {
         let balon = new Image();
         balon.src = this.image;
-        this.ctx.drawImage(balon,this.x, this.y + this.size / 2 , this.size, this.size);
+        this.ctx.drawImage(balon,this.x, this.y - this.size / 2 , this.size, this.size);
       }
     
       setDirection(direction) {
